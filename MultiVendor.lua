d7b43a72026630269ea81471461080127e2786f5
VissaniF <francovissani17@gmail.com>
2024-03-28 21:57:13 -0300

    Multivendedor con Teleport

--- MultiVendor.lua
+++ MultiVendor.lua
@@ -1,20 +1,20 @@
-
-
 local EVENT_ONGOSSIPHELLO       = 1
 local EVENT_ONGOSSIPSELECT      = 2
 
 local NPC_MULTIVENDOR           = 200000
 local VENDOR_ID1                = 350002
-local VENDOR_ID2                = -- insert another vendor id from npc_vendor table here! 
-
--- in case you want to add more vendors just add them via local VENDOR_ID3, local VENDOR_ID4 and so on =)
+local VENDOR_ID2                = 500017
+local VENDOR_ID3                = 500016
+local VENDOR_ID4                = 500015
 
 local function MultiVendorOnGossipHello(event, player, creature)
     player:GossipClearMenu()
-    player:GossipMenuAddItem(1, "Change the text here!", 1, 1)
-    player:GossipMenuAddItem(1, "Change the text here!", 1, 2)
-    -- to add more gossip items just copy a line up above and change the last value in there... so if you copy the line with 2 at the end... make the 2 a 3 and so on =)
-    player:GossipMenuAddItem(1, "Close", 1, 100)
+    player:GossipMenuAddItem(1, "Item Custom", 1, 1)
+    player:GossipMenuAddItem(1, "Cuellos", 1, 2)
+    player:GossipMenuAddItem(1, "Arcos", 1, 3)
+    player:GossipMenuAddItem(1, "Libros", 1, 4)
+    player:GossipMenuAddItem(3, "Teletransportar a Ventormenta", 1, 5) -- Icono, nombre , nose, posicion
+    player:GossipMenuAddItem(4, "Close", 4, 100) -- 
     player:GossipSendMenu(1, creature, 1)
 end
 
@@ -22,9 +22,14 @@ local function MultiVendorOnGossipSelect(event, player, creature, sender, intid,
     if (intid == 1) then
         player:SendListInventory(creature, VENDOR_ID1) 
     elseif (intid == 2) then
-        player:SendListInventory(creature, VENDOR_ID2) 
-        -- to add more selection options just copy the line up above, change the intid according to the last number value at the GossipMenuAddItem and change the vendorid =)
-    
+        player:SendListInventory(creature, VENDOR_ID2)
+    elseif (intid == 3) then
+        player:SendListInventory(creature, VENDOR_ID3)
+    elseif (intid == 4) then
+        player:SendListInventory(creature, VENDOR_ID4)
+    elseif (intid == 5) then
+        -- Teletransporte del jugador a las coordenadas especificadas
+        player:Teleport(0, -8622.457, 774.5229, 96.65421, 0)
     elseif (intid == 100) then
         player:GossipComplete()
     end
