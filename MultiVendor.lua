6ce234fbeccc95c2b82a1952f9a546c7c977bd61
VissaniF <francovissani17@gmail.com>
2024-03-29 00:49:46 -0300

    NPC Profeciones + Boss

--- MultiVendor.lua
+++ MultiVendor.lua
@@ -7,14 +7,49 @@ local VENDOR_ID2                = 500017
 local VENDOR_ID3                = 500016
 local VENDOR_ID4                = 500015
 
+--Encantamiento: 51313
+--Pesca: 45542
+--Alquimia: 51304
+--Herrería: 51300
+--Ingeniería: 51306
+--Herboristería: 50300
+--Inscripción: 45363
+--Joyería: 51311
+--Peletería: 51302
+--Minería: 50310
+--Desuello: 50305
+--Sastrería: 51309
+--Cocina: 51296
+--Primeros auxilios: 51294
+
+
+-- Función para enseñar la profesión de peletería al nivel máximo (450)
+local function TeachAndLevelUpLeatherworking(player)
+    local maxSkillLevel = 450
+    local skillID = 165 -- ID de la habilidad de Peletería
+    local spellID = 51302 -- ID del hechizo para aprender Peletería
+
+    if (player:HasSkill(skillID)) then
+        player:SendBroadcastMessage("¡Ya tienes la profesión de Peletería!")
+    else
+        player:LearnSpell(spellID) -- Aprender el hechizo de Peletería
+        player:AdvanceSkill(skillID, maxSkillLevel) -- Subir la habilidad de peletería al nivel máximo
+        player:SendBroadcastMessage("¡Felicidades! Ahora tienes la profesión de Peletería al nivel máximo.")
+
+        --player:SendBroadcastMessage("¡Todas las recetas de Peletería han sido aprendidas!")
+    end
+end
+
+
 local function MultiVendorOnGossipHello(event, player, creature)
     player:GossipClearMenu()
     player:GossipMenuAddItem(1, "Item Custom", 1, 1)
     player:GossipMenuAddItem(1, "Cuellos", 1, 2)
     player:GossipMenuAddItem(1, "Arcos", 1, 3)
     player:GossipMenuAddItem(1, "Libros", 1, 4)
-    player:GossipMenuAddItem(3, "Teletransportar a Ventormenta", 1, 5) -- Icono, nombre , nose, posicion
-    player:GossipMenuAddItem(4, "Close", 4, 100) -- 
+    player:GossipMenuAddItem(3, "Teletransportar a Ventormenta", 1, 5)
+    player:GossipMenuAddItem(6, "Aprender Peleteria", 1, 6) -- Agregar opción para enseñar Peletería
+    player:GossipMenuAddItem(4, "Close", 4, 100)
     player:GossipSendMenu(1, creature, 1)
 end
 
@@ -28,8 +63,11 @@ local function MultiVendorOnGossipSelect(event, player, creature, sender, intid,
     elseif (intid == 4) then
         player:SendListInventory(creature, VENDOR_ID4)
     elseif (intid == 5) then
-        -- Teletransporte del jugador a las coordenadas especificadas
+        -- Teletransportar al jugador a Ventormenta
         player:Teleport(0, -8622.457, 774.5229, 96.65421, 0)
+    elseif (intid == 6) then
+        -- Enseñar Peletería al jugador y subirla a nivel 450
+        TeachAndLevelUpLeatherworking(player)
     elseif (intid == 100) then
         player:GossipComplete()
     end
--- Penalizacion.lua
+++ Penalizacion.lua
@@ -1,12 +1,22 @@
-local BG_DESERTER_SPELL_ID = 26013 -- ID del hechizo de desertor de campo de batalla
-
-local function RemoveDeserterAura(event, player, _, _, _, _, _, _, _, bgMapId)
-    if bgMapId and player:IsInWorld() then
-        if player:HasAura(BG_DESERTER_SPELL_ID) then
-            player:RemoveAura(BG_DESERTER_SPELL_ID)
-            player:SendBroadcastMessage("You are no longer marked as a deserter.")
-        end
+function OnBGEnd(event, bg, bgId, instanceId, winner)
+    -- Resto del código se mantiene igual
+    
+    -- Otorgar el objeto a los jugadores
+    for k, player in pairs(GetPlayersInWorld()) do
+        player:AddItem(21100, 10)  -- Agrega 10 del objeto con ID 21100 al inventario del jugador
+        player:SendBroadcastMessage("¡Felicidades! Has recibido 10 del objeto 21100 por tu participación en la Batalla de Campo de Batalla.")
     end
 end
 
-RegisterPlayerEvent(29, RemoveDeserterAura) -- Evento 29: PLAYER_BG_DESERTER_AURA_REMOVED
+function OnBGEndDestroy(event, bg, bgId, instanceId)
+    -- Resto del código se mantiene igual
+    
+    -- Otorgar el objeto a los jugadores
+    for k, player in pairs(GetPlayersInWorld()) do
+        player:AddItem(21100, 10)  -- Agrega 10 del objeto con ID 21100 al inventario del jugador
+        player:SendBroadcastMessage("¡Felicidades! Has recibido 10 del objeto 21100 por tu participación en la Batalla de Campo de Batalla.")
+    end
+end
+
+RegisterBGEvent(2, OnBGEnd)
+RegisterBGEvent(4, OnBGEndDestroy)
